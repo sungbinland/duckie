d8ab3bd8dbb6c81a42928694bb5652b23187ecb0
Ji Sungbin <ji@sungb.in>
2022-06-16 15:50:57 +0900

    Delete array.kt

--- shared/domain/src/main/kotlin/land/sungbin/androidprojecttemplate/shared/domain/extension/array.kt
+++
@@ -1,9 +0,0 @@
-package land.sungbin.androidprojecttemplate.shared.domain.extension
-
-fun <T> MutableList<T>.updateIndex(index: Int, newValue: T): MutableList<T> {
-    return apply { set(index, newValue) }
-}
-
-fun <T> MutableList<T>.updateIndex(index: Int, newValueBuilder: T.() -> T): MutableList<T> {
-    return apply { set(index, newValueBuilder(get(index))) }
-}
